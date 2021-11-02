e996143be73a3c30f8abcd6c684f674127bab5b7
wojciechkonieczny <wojciech9929@gmail.com>
2021-11-02 19:52:04 +0100

    Dodanie tłumaczeń Auth w języku PL

---
+++ resources/lang/pl/auth.php
@@ -0,0 +1,45 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Wprowadzono nieprawidłowe poświadczenia',
+    'password' => 'Podane hasło jest niewłaściwe',
+    'throttle' => 'Zbyt duża ilość prób. Spróbuj ponownie za :seconds sekund.',
+    'something_went_wrong' => 'Coś poszło nie tak!',
+    'confirm_pass_info' => 'Wymagane jest dodatkowe uwierzytelnianie poprzez wprowadzenie hasła',
+    'forgot_pass_info' => 'Jeśli nie pamiętasz hasła, wprowadź swój email, na który wyślemy link do zresetowania poświadczeń',
+    'verify_link_info' => 'Link do weryfikacji konta, został wysłany na podany adres email',
+
+    'inputs' => [
+        'email' => 'Email',
+        'password' => 'Hasło',
+        'confirm_pass' => 'Potwierdź hasło',
+        'remember_me' => 'Zapamiętaj mnie',
+        'name' => 'Imię i nazwisko',
+    ],
+
+    'buttons' => [
+        'login' => 'Zaloguj',
+        'register' => "Zarejestruj",
+        'confirm' => 'Potwierdź',
+        'reset_pass_link' => 'Wyślij link',
+        'reset_verification_link' => 'Wyślij link ponownie',
+        'change_pass' => 'Zmień hasło',
+    ],
+
+    'other' => [
+        'forgot_pass' => 'Zapomniałem hasła',
+        'already_registered' => 'Mam już konto'
+    ]
+];
