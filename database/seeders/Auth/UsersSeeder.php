ed9bb6a4b74001bc97811cca19fafd0368239814
wojciechkonieczny <wojciech9929@gmail.com>
2021-11-16 20:34:03 +0100

    Laravel-permission - stworzenie użytkowników i przypisianie ich do ról

--- database/seeders/Auth/UsersSeeder.php
+++ database/seeders/Auth/UsersSeeder.php
@@ -12,12 +12,23 @@ class UsersSeeder extends Seeder {
 
     // ta metoda powinna byc stworzona w kazdym seederze
     public function run() {
-        User::create([
-            'name' => 'wojtekkonieczny',
-            'email' => 'wojtek.konieczny@local',
+
+        $globalAdmin = User::create([
+            'name' => 'admin1',
+            'email' => 'admin1@localhost',
             'email_verified_at' => Carbon::now()->format( 'Y-m-d H:i:s' ),
             'password' => Hash::make( 'zaq1@WSX' )
         ]);
+
+        $normalUser = User::create([
+            'name' => 'wojtekkonieczny',
+            'email' => 'user1@localhost',
+            'email_verified_at' => Carbon::now()->format( 'Y-m-d H:i:s' ),
+            'password' => Hash::make( 'zaq1@WSX' )
+        ]);
+
+        $globalAdmin->assignRole( config('app.admin_role') );
+        $normalUser->assignRole( config('app.user_role') );
     }
 
 }
