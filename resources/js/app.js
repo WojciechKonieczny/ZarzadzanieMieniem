301ffad82c335c7df9b4f6fec0076a9e9ee1806f
wojciechkonieczny <wojciech9929@gmail.com>
2021-11-16 19:49:10 +0100

    jQuery - doinstalowanie pakietu

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,8 @@
 // require('./bootstrap');
 
+// wyeksportowanie globalnie biblioteki jQuery
+global.$ = global.jQuery = require('jquery');
+
 // dolozenie bootstrapa i zestawu ikon bootstrapa
 require('bootstrap');
 require('bootstrap-icons/font/bootstrap-icons.css');
