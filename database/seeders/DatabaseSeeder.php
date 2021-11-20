45f2fbfa49582f75a0b25e2adbb670969e8d7820
wojciechkonieczny <wojciech9929@gmail.com>
2021-11-20 10:09:29 +0100

    Seedery - wywołanie wszystkich

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,13 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Database\Seeders\ItemSeeder;
+use Database\Seeders\CategorySeeder;
+use Database\Seeders\LocationSeeder;
 use Database\Seeders\Auth\RolesSeeder;
 use Database\Seeders\Auth\UsersSeeder;
+use Database\Seeders\ModelOrNameSeeder;
+use Database\Seeders\ManufacturerSeeder;
 use Database\Seeders\Auth\PermissionsSeeder;
 
 class DatabaseSeeder extends Seeder
@@ -19,5 +24,10 @@ class DatabaseSeeder extends Seeder
         $this->call(RolesSeeder::class);
         $this->call(PermissionsSeeder::class);
         $this->call(UsersSeeder::class);
+
+        $this->call(CategorySeeder::class);
+        $this->call(ManufacturerSeeder::class);
+        $this->call(ModelOrNameSeeder::class);
+        $this->call(ItemSeeder::class);
     }
 }
