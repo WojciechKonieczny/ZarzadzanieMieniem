a0c18ad6a38dff417a463a36b81edf712e54bed3
wojciechkonieczny <wojciech9929@gmail.com>
2021-11-24 00:59:47 +0100

    Manufacturers - edycja widoku

---
+++ resources/views/manufacturers/index.blade.php
@@ -0,0 +1,42 @@
+{{-- szablon bedziie wykorzystywal app.blade.php --}} 
+<x-app-layout>
+    <x-slot name="styles">
+        <link rel="stylesheet" href="{{ asset('css/manufacturers.css') }}">
+    </x-slot>
+
+    <x-slot name="scripts">
+        <script src="{{ asset('js/manufacturers.js') }}"></script>
+    </x-slot>
+
+    <div class="container">
+        <h1>{{ __('translations.manufacturers.title') }}</h1>
+
+        <div class="table-container">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>{{ __('translations.manufacturers.attribute.name') }}</th>
+                        <th>{{ __('translations.manufacturers.attribute.count') }}</th>
+                        <th>{{ __('translations.attribute.created_at') }}</th>
+                        <th>{{ __('translations.attribute.updated_at') }}</th>
+                        <th>{{ __('translations.attribute.deleted_at') }}</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    @foreach($manufacturers as $manufacturer)
+                    <tr>
+                        <td> {{ $manufacturer->id }} </td>
+                        <td> {{ $manufacturer->name }} </td>
+                        <td> {{ $manufacturer->items_count }} </td>
+                        <td> {{ $manufacturer->created_at }} </td>
+                        <td> {{ $manufacturer->updated_at }} </td>
+                        <td> {{ $manufacturer->deleted_at }} </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+</x-app-layout>
