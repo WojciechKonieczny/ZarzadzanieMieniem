1e8060c728da32c06ffe7590bd24875d8ee8e148
wojciechkonieczny <wojciech9929@gmail.com>
2021-12-16 00:23:08 +0100

    Users - poprawa migracji i dołożenie softDeletes

--- database/migrations/2021_11_18_161447_create_items_table.php
+++ database/migrations/2021_11_18_161447_create_items_table.php
@@ -17,13 +17,13 @@ class CreateItemsTable extends Migration
             $table->id();
 
             $table->unsignedBigInteger('manufacturer_id');
-            $table->foreign('manufacturer_id')->references('id')->on('manufacturers')->onDelete('no action');
+            $table->foreign('manufacturer_id')->references('id')->on('manufacturers')->onDelete('cascade');
 
             $table->unsignedBigInteger('model_or_name_id');
-            $table->foreign('model_or_name_id')->references('id')->on('model_or_names')->onDelete('no action');
+            $table->foreign('model_or_name_id')->references('id')->on('model_or_names')->onDelete('cascade');
             
             $table->unsignedBigInteger('category_id');
-            $table->foreign('category_id')->references('id')->on('categories')->onDelete('no action');
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
             
             $table->timestamps();
             $table->softDeletes(); // dolozenie kolumny `deleted at`, domyslnie jest NULL; gdy go usuwamy, to wkladana jest data; kolumna "ukryta"
